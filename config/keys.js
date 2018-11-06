80b73fa6fa2e4e8ca0615d3322f5be9f450fc55d
Aleksandar <aleksandermihaylov@gmail.com>
2018-11-06 10:12:15 +0200

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://alek1339:sasho777@ds249873.mlab.com:49873/cars',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod')
+} else {
+  module.exports = require('./keys_dev')
 }
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://alek1339:sasho777@ds249873.mlab.com:49873/cars',
+    secretOrKey: 'secret'
+}
